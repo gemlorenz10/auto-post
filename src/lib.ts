c729b3a1212757c3a0d21f05cccb0e6e25f5bb9a
gemlorenz10 <gemlorenz10@gmail.com>
2017-10-18 21:38:00 +0800

    rename modules

--- src/lib.ts
+++ src/lib.ts
@@ -1,51 +1,82 @@
 const $ = require('cheerio');
 
+export class Utility{
+    constructor(){}
+    /**
+     * 
+     * @param html - must be an HTML string.
+     * 
+     */
+    textify(html: string): string {
+        let re = '';
 
-/**
- * 
- * @param html - must be an HTML string.
- * 
- */
-export function textify(html: string): string {
-    let re = '';
+        html = html.replace(/\r/g, '');
+        html = html.replace(/\n/g, '');
+        html = html.replace(/\s+/g, ' ');
 
-    html = html.replace(/\r/g, '');
-    html = html.replace(/\n/g, '');
-    html = html.replace(/\s+/g, ' ');
-
-    html = html.replace(/<p>/ig, '');
-    html = html.replace(/<\/p>/ig, "\n");
-    html = html.replace(/<div>/ig, '');
-    html = html.replace(/<\/div>/ig, "\n");
-
-
-    const $html = $.load(html)('body');
+        html = html.replace(/<p>/ig, '');
+        html = html.replace(/<\/p>/ig, "\n");
+        html = html.replace(/<div>/ig, '');
+        html = html.replace(/<\/div>/ig, "\n");
 
 
+        const $html = $.load(html)('body');
 
-    re = $html.text();
 
 
-    const $a = $html.find('a');
-    let aDone = [];
-    if ($a.length) {
-        $a.each((i, e) => {
-            const txt = $(e).text();
-            if (aDone.indexOf(txt) !== -1) return;
-            const url = $(e).prop('href');
-            re = re.split(txt).join(`${txt} (${url}) `);
-            aDone.push(txt);
-        });
+        re = $html.text();
+
+
+        const $a = $html.find('a');
+        let aDone = [];
+        if ($a.length) {
+            $a.each((i, e) => {
+                const txt = $(e).text();
+                if (aDone.indexOf(txt) !== -1) return;
+                const url = $(e).prop('href');
+                re = re.split(txt).join(`${txt} (${url}) `);
+                aDone.push(txt);
+            });
+        }
+
+        // console.log(aDone);
+
+        return re;
+    }
+    /**
+     * Returns time in year, month, date, hour, minutes, seconds(2017-10-6-17-24-58)
+     */
+    timeStamp() {
+        let date = new Date;
+        return [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()].join('-');
+    }
+}
+
+export class Log {
+    success( message = 'Script successful.' ) {
+        this.exitProcess('success', message, );
     }
 
-    // console.log(aDone);
+    fail( message = 'Script failed!' ) {
+        this.exitProcess('fail', message, );
+    }
+    /**
+     * Closes the script. If message is equal to null, script will exit without logging.
+     * 
+     * @param message
+     * @param code default code is success.
+     */
+    private exitProcess( code: string, message?: string ) {
+        if ( code = 'success' ) {
+            if ( message ) process.stdout.write('Success: ' + message);
+            process.exit(0);
+        } else if ( code = 'fail' ) {
+            if ( message ) process.stderr.write('Error: ' + message);
+            process.exit(0);
+        } else {
+            process.stderr.write('UNHANDLED CODE!');
+            process.exit(0);
+        }
+    }
 
-    return re;
-}
-/**
- * Returns time in year, month, date, hour, minutes, seconds(2017-10-6-17-24-58)
- */
-export function timeStamp() {
-    let date = new Date;
-    return [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()].join('-');
 }
--- src/task/facebook.ts
+++ src/task/facebook.ts
@@ -1,18 +1,20 @@
+import { Utility, Log } from '../lib';
 import { MyNightmare as Nightmare } from '../nightmare/nightmare';
 import * as path from 'path'
-import * as lib from '../lib';
 import * as fs from 'fs';
-declare let protocol; // temporary
+// declare let protocol; // temporary
 const argv = require('yargs').string('category').argv;
 
-if (argv.pid === void 0) { console.log('no pid'); process.exit(1); }
-protocol.set(argv.pid);
-protocol.send('begin', (new Date).toLocaleTimeString());
+const util = new Utility();
+const log = new Log();
+// if (argv.pid === void 0) { console.log('no pid'); process.exit(1); }
+// protocol.set(argv.pid);
+// protocol.send('begin', (new Date).toLocaleTimeString());
 
 class Facebook extends Nightmare {
 
     private serverUrl = 'https://m.facebook.com'
-    private post = null;
+    private post = 'Hello!';
 
     private id = argv.id;
     private password = argv.password;
@@ -28,22 +30,20 @@ class Facebook extends Nightmare {
      */
     async main() {
         //  get data from firebase        
-        if (this.post === null) protocol.end('fail', 'failed to get post from firebase');
-        else protocol.send('got post from firebase');
+        // if (this.post === null) log.fail('failed to get post from firebase');
+        // else protocol.send('got post from firebase');
 
         await this.login();
         await this.publish();
 
-        protocol.success();
+       log.success();
     }
 
     private async login() {
         let $html = await this.get(this.serverUrl);
 
-        protocol.send('Waiting for email field.');
         let emailField = await this.waitAppear('input[name="email"]', 3);
         if( !emailField ){
-            protocol.send('No email field! 2nd try login...')
             let loginPage = await this.waitAppear(`a[href='/login/?ref=dbl&fl&refid=8']`, 3);
             if(!loginPage) this.captureError('Unhandled page.')
             await this.get( this.serverUrl + '/login/?ref=dbl&fl&refid=8' );
@@ -52,7 +52,6 @@ class Facebook extends Nightmare {
         emailField = await this.waitAppear('input[name="email"]', 3)
         if (!emailField) this.captureError('Unknown Error.')
         
-        protocol.send('logging in ... ');
         await this.insert('input[name="email"]', '');
         await this.wait(800)
         await this.insert('input[name="email"]', this.id);
@@ -61,47 +60,38 @@ class Facebook extends Nightmare {
         await this.insert('input[name="pass"]', this.password);
         await this.click('input[name="login"]');
 
-        protocol.send('Waiting to redirect.');
         let re = await this.waitDisappear('input[name="pass"]', 5);
         if (!re) this.captureError('Still in login page after timeout!.', );
 
-        protocol.send('Redirecting to home page.');
         await this.get(this.serverUrl);
 
-        protocol.send('Checking if successfully logged in.')
         let isLogin = await this.waitAppear(`a:contains('Logout')`, 5);
         if (!isLogin) await this.captureError('Failed login.')
-        protocol.send('login', 'success');
     }
     /**
      * For publishing on facebook.
      */
     private async publish() {
         // shaping the post
-        let content = this.post.title + '\n' + lib.textify(this.post.content);
+        let content = util.textify(this.post);
         let postThis = content.trim();
 
-        protocol.send('Opening Group: ', argv.category);
-        await this.get(this.serverUrl + '/groups/' + this.argv.category);
+        // await this.get(this.serverUrl + '/groups/' + this.argv.category);
 
-        let isGroupOpen = await this.waitAppear('a[name=groupMenuBottom]', 5);
-        if (!isGroupOpen) await this.captureError('captureError on opening group page.');
-        protocol.send('Opening :' + argv.category, 'success!')
+        // let isGroupOpen = await this.waitAppear('a[name=groupMenuBottom]', 5);
+        // if (!isGroupOpen) await this.captureError('captureError on opening group page.');
 
-        protocol.send('checking post text area');
         let canPost = await this.waitAppear('textarea[name="xc_message"]');
         if (!canPost) await this.captureError('Cant find textarea to post.');
-        protocol.send('checking post text area', 'text area found!')
 
-        protocol.send('Typing the post: ', 'typing..');
         await this.insert('textarea[name="xc_message"]', postThis);
         await this.click('input[name="view_post"]');
 
         // Verify post if posted.
         // pending?
-        protocol.send('Verify post if posted')
+
         let isPending = await this.waitAppear(`a:contains('1 post requiring approval')`, 5);
-        if (isPending) protocol.end('post', 'Post pending.'); // or delete old and post another.
+        if (isPending) log.fail('Post pending.'); // or delete old and post another.
         // posted?
         let isPosted = await this.findPost(postThis);
         if (!isPosted) await this.captureError('Post not found.');
@@ -119,17 +109,16 @@ class Facebook extends Nightmare {
         return re;
     }
     /**
-     * It captures the current screen state and fires 'protocol.end()' closing the script.
+     * It captures the current screen state and fires 'lib.exitProcess()' closing the script.
      * @param message 
      * @param filePath - where to save the captured image 
      * @param fileName - filename of the image.
      */
-    private async captureError( message, filePath = path.join(__dirname, '..', 'screenshot'), fileName = lib.timeStamp() + '-facebook.png' ){
+    private async captureError( message, filePath = path.join(__dirname, '..', 'screenshot'), fileName = util.timeStamp() + '-facebook.png' ){
         
         if (!fs.existsSync(filePath)) fs.mkdirSync(filePath);
-        
         await this.screenshot( path.join(filePath, fileName) );
-        protocol.fail(message + 'Check screenshot at :' + path.join(filePath, fileName) );    
+        log.fail(message + 'Check screenshot at :' + path.join(filePath, fileName) );    
 
     }   
 
@@ -137,7 +126,7 @@ class Facebook extends Nightmare {
 
 
 let options = {
-    show: argv.browser === 'true',
+    show: false,
     x: 1408, y: 0, width: 360, height: 700,
     openDevTools: { mode: '' },
 };
